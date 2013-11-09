15f1d553aa289365567eda296e844d5cfa3aefaf
Diego José Fernández Hernández <diego@diegofer.com>
2013-11-09 17:30:19 +0100

    Implemented unscrambler

--- main.cpp
+++ main.cpp
@@ -4,7 +4,24 @@
 using namespace std;
 
 int main(int argc, char ** argv){
-	cout << "Hello world!" << endl;
+	if (argc < 2){
+		cout << "encoded data not found!" << endl;
+		return 0;
+	}
+
+	string data = string(argv[1]);
+	string content = "";
+	uint32_t key = 50;
+	
+	for (int i = 0; i < data.length(); i += 2){
+		uint32_t c = stoul(data.substr(i, 2), NULL, 16) ^ key;
+		content += (char)c;
+		
+		key = key + c & 0xFF;
+	}
+	content = string(content.rbegin(), content.rend());
+	
+	cout << content << endl;
 	
 	return 0;
 }
